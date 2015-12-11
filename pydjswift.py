30e8edab3f0250c76a894034f9fbb219c2d6dd01
inso <thomas.moussajee@gmail.com>
2015-12-11 09:02:35 +0100

    generate swift too

--- pydjswift.py
+++ pydjswift.py
@@ -5,23 +5,23 @@ from ParsePyModel import *
 def getTypeSwift(varType):
     if "ForeignKey" in varType :
         typeConverted = varType.split("(")[1].replace("'", "").replace(")", "")
-        codeImport = "@import " + typeConverted + ".h" + "\n"
+        codeImport = "import " + typeConverted + "\n"
         return typeConverted, codeImport
     
-    typeTable = { "models.CharField" : "NSString",
-                  "models.TextField" : "NSString",
-                  "models.IntegerField" : "NSInteger",
-                  "models.DecimalField": "NSInteger",
-                  "models.PositiveSmallIntegerField" : "NSInteger",
-                  "models.BigIntegerField" : "NSInteger",
-                  "models.BooleanField" : "NSInteger",
-                  "models.DateField" : "NSDate",
-                  "models.DateTimeField" : "NSDate",
+    typeTable = { "models.CharField" : "String",
+                  "models.TextField" : "String",
+                  "models.IntegerField" : "Int",
+                  "models.DecimalField": "Int",
+                  "models.PositiveSmallIntegerField" : "Int",
+                  "models.BigIntegerField" : "Double",
+                  "models.BooleanField" : "Bool",
+                  "models.DateField" : "Date",
+                  "models.DateTimeField" : "Date",
     }
     typeConverted = typeTable.get(varType)
     if typeConverted is None:
         print varType + " not found"
-        return "NSInteger", None
+        return "String", None
     return typeConverted , None
 
 def header():
@@ -31,3 +31,33 @@ def header():
     code += "//\n"
     return code
 
+def generateObjc(parsed):
+    for model in parsed:
+        swiftFile = open("output/" + model.nameClass + ".swift", "w")
+        
+        code = ""
+        codeCor = ""
+        codeImport = ""
+
+        codeCor += "Class " + model.nameClass + " {\n\n"
+        
+        for varName, varType in  model.var.iteritems():
+            typeVar, newImport = getTypeSwift(varType)
+
+            if newImport != None:
+                codeImport += newImport 
+
+            codeCor += "\tlet " + varName + ": " + typeVar + "\n"
+        codeCor += "\t}" 
+            
+        code += header() + "\n"
+        code += codeImport + "\n"
+        code += codeCor
+
+        swiftFile.write(code)
+
+def main():
+    n = ParsePyModel("out.py")
+    generateObjc(n.parse())
+
+main()
